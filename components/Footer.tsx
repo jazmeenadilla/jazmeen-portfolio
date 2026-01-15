d1d783258878b5333321cfd9d2c9c69a2c4044b6
Maliskandar <sao19082002@gmail.com>
2026-01-15 12:46:35 +0700

    Replace Text w

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="bg-slate-950 border-t border-slate-900 py-8 text-center text-slate-500 text-sm">
             <p>
                 © {new Date().getFullYear()} Lumevo. All rights reserved | Built with <span className="text-teal-400">Next.js</span> & <span className="text-blue-400">Tailwind</span>.
-                <br className="md:hidden" /> Data Storyteller Portfolio.
+                <br className="md:hidden" /> Data Analyst Portfolio.
             </p>
         </footer>
     );
